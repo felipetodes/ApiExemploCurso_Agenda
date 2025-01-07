5cfe8ac14dac6587967c5f4da3d9b217d707f35e
Felipe Todeschini <todeschini1992@gmail.com>
2025-01-07 19:02:56 -0300

    Adicionar arquivos de projeto.

---
+++ Contato.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiExemploCurso
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Email { get; set; }
+
+        public DateTime DtInc { get; set; }
+    }
+}
---
+++ Controllers/AgendaController.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExemploCurso.Controllers
+{
+    [ApiController]
+    [Route("Agenda")]
+
+    public class AgendaController : Controller
+    {
+        private readonly string _connectionString;
+
+        public AgendaController(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("MinhaConexao");
+        }
+
+        [HttpGet]
+        [Route("ExecutarSelect")]
+        public ActionResult ExecutarSelect() {
+
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT ID, NOME, EMAIL, DT_INC FROM CONTATO";
+
+                con.Open();
+
+                var command = new SqlCommand(sql, con);
+                var reader = command.ExecuteReader();
+                var lista = new List<Contato>();
+            }
+        }
+
+       
+    }
+}

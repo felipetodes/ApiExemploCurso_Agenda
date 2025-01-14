869022e7214e7a9b19354be2db02e5ff41f41ac3
Felipe Todeschini <todeschini1992@gmail.com>
2025-01-13 21:36:23 -0300

    atualizações

--- Conexao.cs
+++ Conexao.cs
@@ -1,6 +1,13 @@
 ﻿namespace ApiExemploCurso
 {
-    public class Conexao
+    public static class Conexao
     {
+        private static readonly Dictionary<string , string> conexoes = new Dictionary<string , string>();
+        static Conexao()
+        {
+            
+        }
+
+        public static  string AGENDA { get; set; }
     }
 }

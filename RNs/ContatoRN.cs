ed530bc90b025aec2e28b3c9e2d30cc8fd49274f
Felipe Todeschini <todeschini1992@gmail.com>
2025-01-15 20:00:03 -0300

    atualização

--- RNs/ContatoRN.cs
+++ RNs/ContatoRN.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data.SqlClient;
 using ApiExemploCurso.EDs;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiExemploCurso.RNs
@@ -14,37 +15,15 @@ namespace ApiExemploCurso.RNs
         {
                 using (var con = new SqlConnection(Conexao.AGENDA))
                 {
-                    string sql = "SELECT ID, NOME, EMAIL, DT_INC FROM CONTATO";
+                con.Open();
+                string sql = "SELECT ID, NOME, EMAIL, DT_INC FROM CONTATO";
 
-                    con.Open();
+                   return con.Query<ContatoED>(sql).ToList();
+                
 
-                    var command = new SqlCommand(sql, con);
-                    var reader = command.ExecuteReader();
-                    var lista = new List<ContatoED>();
-
-                    while (reader.Read())
-                    {
-                        lista.Add(
-                            new ContatoED()
-                            {
-                                Id = reader.GetInt32(0),
-                                Nome = reader.GetString(1),
-                                Email = reader.GetString(2),
-                                DtInc = reader.GetDateTime(3)
-
-                            }
-                            );
-
-                    }
-
-                    con.Close();
-
-                    if (lista.Count > 0)
-                        return Ok(lista);
-                    return NotFound("Nenhum contato localizado!");
                 }
 
             }
         }
     }
-}
+
